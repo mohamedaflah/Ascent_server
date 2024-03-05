d1beb072d6782a3154c45bf47619596acab4562a
Aflu <mohamedaflah186@gmail.com>
2024-03-05 14:30:52 +0530

    d

--- services/company/src/handlers/routers/companyRoute.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-
-const companyRoute=Router()
-
-companyRoute.get("/edit-profile/:companyId")
--- services/company/src/index.ts
+++ services/company/src/index.ts
@@ -1,18 +1,22 @@
 import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 const app = express();
 
-
-
-
+app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL as string,
+    origin: [process.env.CLIENT_URL as string],
     credentials: true,
   })
 );
 
-app.use(cookieParser());
+app.use()
+
+app.listen(process.env.COMPANY_SERVICE_PORT, () =>
+  console.log(
+    ` Company Service started at ${process.env.COMPANY_SERVICE_PORT} `
+  )
+);
