cdd54ec6cc255dc129afceea51e80926d62c7d08
Aflu <mohamedaflah186@gmail.com>
2024-04-14 14:54:14 +0530

    deploy:__

--- services/auth/src/infra/message/kafka/index.ts
+++ services/auth/src/infra/message/kafka/index.ts
@@ -1,5 +1,10 @@
 import { Kafka, Producer, Consumer } from "kafkajs";
 
+
+console.log(process.env.KAFKA_CLIENT_ID)
+console.log(process.env.KAFKA_BROKER_URL)
+console.log(process.env.KAFKA_USER_NAME)
+console.log(process.env.KAFKA_PASSWORD)
 const kafka = new Kafka({
   clientId: String(process.env.KAFKA_CLIENT_ID),
   brokers: [String(process.env.KAFKA_BROKER_URL)],
