996b86a779875396d8acc42853f80404f343d818
Aflu <mohamedaflah186@gmail.com>
2024-07-27 19:49:42 +0530

    feat: updated

--- services/auth/src/infra/message/kafka/index.ts
+++ services/auth/src/infra/message/kafka/index.ts
@@ -103,8 +103,8 @@ f0N6IS1hK6QACR0GvMyx7/Fjtv2k
 const sslOptions = {
   rejectUnauthorized: true,
   ca: [CA],
-  key: SERVICE_KEY,
-  cert: SERVER_CERT,
+  // key: SERVICE_KEY,
+  // cert: SERVER_CERT,
 };
 
 const kafka = new Kafka({
