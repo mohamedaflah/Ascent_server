e26690bc80b05212ce90355e083c4d815bf778d7
Aflu <mohamedaflah186@gmail.com>
2024-07-29 16:28:11 +0530

    refactor: user updated

--- services/user/src/middlewares/checkAuthentication.ts
+++ services/user/src/middlewares/checkAuthentication.ts
@@ -1,19 +1,34 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from 'jsonwebtoken'
-export const checkAuthentication=async(req:Request,res:Response,next:NextFunction) => {
-    console.log("🚀 ~ checkAuthentication ~ req:",req.cookies)
-    
-    
-    const token=req.cookies.access_token
-    if(!token){
-        return res.status(400).json({status:false,user:null,message:"not autherized"})
+import jwt from "jsonwebtoken";
+export const checkAuthentication = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log("🚀 ~ checkAuthentication ~ req:", req.cookies);
+
+  const authHeader = req.headers.authorization;
+  const secondarytoken = authHeader && authHeader.split(" ")[1];
+
+  const token = req.cookies.access_token || secondarytoken;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ status: false, user: null, message: "not autherized" });
+  }
+  jwt.verify(
+    token,
+    String(process.env.JWT_KEY),
+    (err: Error | any, decoded: any) => {
+      if (err) throw err;
+      if (decoded) {
+        next();
+      } else {
+        return res
+          .status(400)
+          .json({ status: false, user: null, message: "not autherized" });
+      }
     }
-    jwt.verify(token,String(process.env.JWT_KEY),(err:Error|any,decoded:any)=>{
-        if(err) throw err
-        if(decoded){
-            next()
-        }else{
-            return res.status(400).json({status:false,user:null,message:"not autherized"})
-        }
-    })
-}
+  );
+};
